85f947d2ffe5d1f218682ded787ba13bafce590d
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2023-04-12 19:08:37 +0100

    create logoIgnite create layouts modife style for header and History create icons for home and history

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoignite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <header></header>
+  return (
+    <HeaderContainer>
+      <img src={logoignite} />
+      <nav>
+        <NavLink to="" title="Timer">
+          {<Timer size={24} />}
+        </NavLink>
+        <NavLink to="/history" title="history">
+          {<Scroll size={24} />}
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
